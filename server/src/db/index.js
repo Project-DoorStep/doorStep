42d78242152ba4ff9d6ef96cdd0cf6b94c804e92
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-11-03 13:29:45 +0530

    Api Error Response and Async handler

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js'
 
+
 const connectDB = async () => {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGO.URI}/${DB_NAME}`);
-    console.log(`MONGO DB CONNECTED :: DB HOST :: ${connectionInstance.connection.host} :: PORT :: ${connectionInstance.connection.port} :: NAME :: ${connectionInstance.connection.name}`);
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO.URI}/${DB_NAME}`);
+        console.log(`MONGO DB CONNECTED :: DB HOST :: ${connectionInstance.connection.host} :: PORT :: ${connectionInstance.connection.port} :: NAME :: ${connectionInstance.connection.name}`);
+    } catch (error) {
+        
+    }
 
     //errors 
     // console.log(`MONGODB CONNECTION FAILED :: ${error}`);
